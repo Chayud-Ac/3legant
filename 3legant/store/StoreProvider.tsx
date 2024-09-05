ba7d9dd3f9c334c97a45c735d63e46360f41217a
Chayud-Ac <acieacie@hotmail.com>
2024-09-05 19:36:56 +0700

    remove createAsyncChunck and apply normal datafetching and use dispath to set the initial state of cart and wishlist

--- 3legant/store/StoreProvider.tsx
+++ 3legant/store/StoreProvider.tsx
@@ -2,10 +2,10 @@
 import { ReactNode, useEffect } from "react";
 import { RootState, store } from "./store";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { fetchCart } from "./slices/cartSlice";
+import { setCart } from "./slices/cartSlice";
 import { AppDispatch } from "./store";
 import { setUser } from "./slices/userSlice";
-import { fetchWishList } from "./slices/wishListSlice";
+import { setWishlist } from "./slices/wishListSlice";
 
 // create StoreProvider and define it as a client component
 // ถ้าเรา follow ตาม document โดยไม่สร้าง Provider แยก มันจะทำให้ application ทั้งหมด กลายเป็น client component เราจะ สูญเสีย SSR ไป
@@ -15,14 +15,46 @@ type StoreProviderProps = {
   session: any; // The session object passed down from AuthProvider
 };
 
+const fetchCartData = async (userId: string) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/${userId}?q=cart`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch cart");
+  }
+  const { data } = await response.json();
+  return data;
+};
+
+const fetchWishlistData = async (userId: string) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/${userId}?q=wishlist`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch wishlist");
+  }
+  const { data } = await response.json();
+  return data;
+};
+
 // StoreProviderComponent นี้ handle การดึง ข้อมูล cart จาก backend เพื่อเซ็ทค่า initial cart เป็นค่าที่อยู่ใน DB
 const StoreProviderComponent = ({ session }: { session: any }) => {
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     if (session?.user?.id) {
-      dispatch(fetchCart(session.user.id));
-      dispatch(fetchWishList(session.user.id));
+      const fetchData = async () => {
+        try {
+          const cartData = await fetchCartData(session?.user?.id);
+          dispatch(setCart(cartData));
+
+          const wishlistData = await fetchWishlistData(session?.user?.id);
+          dispatch(setWishlist(wishlistData));
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+
       dispatch(
         setUser({
           id: session.user.id,
@@ -31,6 +63,8 @@ const StoreProviderComponent = ({ session }: { session: any }) => {
           image: session.user.image,
         })
       );
+
+      fetchData();
     }
   }, [dispatch, session]);
 
